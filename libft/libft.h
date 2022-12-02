27f898e757a583ded3030cbc8070ee061c4703ee
Sanghun <po4865@gmail.com>
2022-12-03 00:29:47 +0100

    feat: Create and add header file.

---
+++ libft/libft.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sanghupa <sanghupa@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/02 16:00:38 by sanghupa          #+#    #+#             */
+/*   Updated: 2022/12/03 00:25:46 by sanghupa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+
+# define INT_MAX 2147483647
+# define INT_MIN -2147483648
+# define INT_MAX_STR "2147483647"
+# define INT_MIN_STR "-2147483648"
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
+int		ft_isdigit(int c);
+int		ft_isalpha(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+
+char	*ft_strjoin(char const *s1, char const *s2);
+
+#endif

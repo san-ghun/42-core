13438679ea127b26de802161d9e71ec0b992997b
Sanghun <po4865@gmail.com>
2022-12-03 00:29:10 +0100

    feat: Add some .c files

---
+++ libft/ft_strjoin.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sanghupa <sanghupa@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/02 23:19:49 by sanghupa          #+#    #+#             */
+/*   Updated: 2022/12/02 23:38:55 by sanghupa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlen(const char *s)
+{
+	int	len;
+	int	i;
+
+	len = 0;
+	i = 0;
+	while (s[i] != 0)
+	{
+		len++;
+		i++;
+	}
+	return (len);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	i;
+	size_t	j;
+	char	*tmp;
+
+	i = 0;
+	j = 0;
+	tmp = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!tmp)
+		return (NULL);
+	while (s1[i])
+	{
+		tmp[i] = s1[i];
+		i++;
+	}
+	while (s2[i])
+		tmp[i++] = s2[j++];
+	tmp[i] = '\0';
+	return (tmp);
+}

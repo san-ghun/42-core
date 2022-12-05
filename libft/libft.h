ee0f7f74722e0099ef2a902fd6dd74e110b749e7
Sanghun <po4865@gmail.com>
2022-12-05 13:59:25 +0100

    doc: Update libft.h - Add documentation comments to classify functions.

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: sanghupa <sanghupa@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 16:00:38 by sanghupa          #+#    #+#             */
-/*   Updated: 2022/12/05 13:47:37 by sanghupa         ###   ########.fr       */
+/*   Updated: 2022/12/05 13:57:34 by sanghupa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,24 +26,28 @@ typedef struct s_list
 	struct s_list	*next;
 }					t_list;
 
+// is* functions
 int		ft_isdigit(int c);
 int		ft_isalpha(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
+// to* functions
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+// string functions
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
-int		ft_toupper(int c);
-int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *str);
-
 char	*ft_strdup(const char *s1);
-
+// 
 char	*ft_strjoin(char const *s1, char const *s2);
+// memory functions
+// put_fd functions
 
 #endif

c5699082467db6f652903b18baac0d8ba36128a2
Sanghun <po4865@gmail.com>
2022-12-07 02:05:14 +0100

    feat: Add ft_split.c
    - Update and complete ft_split().

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,11 +6,53 @@
 /*   By: sanghupa <sanghupa@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 13:04:31 by sanghupa          #+#    #+#             */
-/*   Updated: 2022/12/06 23:50:16 by sanghupa         ###   ########.fr       */
+/*   Updated: 2022/12/07 02:01:23 by sanghupa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static size_t	ft_split_size(char const *s, char c)
+{
+	size_t	len;
+
+	len = 0;
+	while (*s)
+	{
+		if (*s != c)
+		{
+			len++;
+			while (*s && *s != c)
+				s++;
+		}
+		if (*s)
+			s++;
+	}
+	return (len);
+}
+
+char	**ft_split_process(char const *s, char c, char **tmp, size_t i)
+{
+	char	*start;
+
+	while (*s)
+	{
+		if (*s != c)
+		{
+			start = (char *)s;
+			while (*s && *s != c)
+				s++;
+			tmp[i] = (char *)malloc(sizeof(char) * (s - start + 1));
+			if (!tmp[i])
+				return (NULL);
+			ft_strlcpy(tmp[i++], start, s - start + 1);
+		}
+		if (*s)
+			s++;
+	}
+	tmp[i] = NULL;
+	return (tmp);
+}
 
 /// @brief allocate with malloc() and returns an array of strings 
 /// obtained by splitting `s` using the character `c` as a delimiter. 
@@ -22,17 +64,14 @@
 char	**ft_split(char const *s, char c)
 {
 	char	**tmp;
-	char	*start;
 	size_t	i;
 
-	if (!s || !c)
+	if (!s)
 		return (NULL);
-	// calculate the length of array, and allocate mem to **tmp.
-	// NULL if the allocation fails.
-	// iterate through s and check *s == c.
-	// if find delimiter, make a chunk of string ending with NULL pointer.
-		// insert chunk of string to the array.
-	// else iterate and pass through.
-	// end the array with NULL pointer.
+	tmp = (char **)malloc(sizeof(char *) * (ft_split_size(s, c) + 1));
+	if (!tmp)
+		return (NULL);
+	i = 0;
+	tmp = ft_split_process(s, c, tmp, i);
 	return (tmp);
 }

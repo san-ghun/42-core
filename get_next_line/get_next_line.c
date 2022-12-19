4f91298136d28d51c4f43f6765e528c2ceda517e
Sanghun <po4865@gmail.com>
2022-12-19 14:02:30 +0100

    init: Initiate new project get_next_line

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sanghupa <sanghupa@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/16 23:22:56 by sanghupa          #+#    #+#             */
+/*   Updated: 2022/12/16 23:30:46 by sanghupa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+/// @brief return a line read from a file descriptor `fd`.
+/// @param fd the file descriptor to read from.
+/// @return read line if correctly behave, 
+/// or a NULL if there is nothing else to read, or an error occurred.
+char	*get_next_line(int fd)
+{
+	return (NULL);
+}

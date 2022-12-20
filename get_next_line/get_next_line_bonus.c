f33378589b65c746f350cefd3f165517be236a8b
Sanghun Park <sanghupa@student.42berlin.de>
2022-12-20 13:22:48 +0100

    update: add _bonus files

--- get_next_line/get_next_line_bonus.c
+++ get_next_line/get_next_line_bonus.c
@@ -1,16 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sanghupa <sanghupa@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 23:22:56 by sanghupa          #+#    #+#             */
-/*   Updated: 2022/12/20 13:22:25 by sanghupa         ###   ########.fr       */
+/*   Updated: 2022/12/20 12:16:52 by sanghupa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 /// @brief return a line read from a file descriptor `fd`.
 /// @param fd the file descriptor to read from.
@@ -20,24 +20,3 @@ char	*get_next_line(int fd)
 {
 	return (NULL);
 }
-/*
-#include <fcntl.h>
-#include <stdio.h>
-#include <string.h>
-int	main(int ac, char **av)
-{
-	int		fd;
-	char	*line;
-
-	fd = open("", O_RDONLY);
-	line = get_next_line(fd);
-
-	strcpy(src, "This is an example.");
-	res_org = strchr(src, ch);
-	res_ft = ft_strchr(src, ch);
-	printf("Origin string is |%s| \n", src);
-	printf("src: String after |%c| is - |%s|\n", ch, res_org);
-	printf("ft_: String after |%c| is - |%s|\n\n", ch, res_ft);
-	return (0);
-}
-*/
---
+++ get_next_line/get_next_line_bonus.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sanghupa <sanghupa@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/16 23:23:40 by sanghupa          #+#    #+#             */
+/*   Updated: 2022/12/20 12:16:31 by sanghupa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+
+#endif

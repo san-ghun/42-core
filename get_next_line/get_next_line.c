2bd472e564ad99664e187140e2d7c7991a7c0f4a
Sanghun Park <sanghupa@student.42berlin.de>
2022-12-20 13:17:03 +0100

    update: some and working

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: sanghupa <sanghupa@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 23:22:56 by sanghupa          #+#    #+#             */
-/*   Updated: 2022/12/16 23:30:46 by sanghupa         ###   ########.fr       */
+/*   Updated: 2022/12/20 12:28:20 by sanghupa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,24 @@
 char	*get_next_line(int fd)
 {
 	return (NULL);
+}
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+int	main(int ac, char **av)
+{
+	int		fd;
+	char	*line;
+
+	fd = open("", O_RDONLY);
+	line = get_next_line(fd);
+
+	strcpy(src, "This is an example.");
+	res_org = strchr(src, ch);
+	res_ft = ft_strchr(src, ch);
+	printf("Origin string is |%s| \n", src);
+	printf("src: String after |%c| is - |%s|\n", ch, res_org);
+	printf("ft_: String after |%c| is - |%s|\n\n", ch, res_ft);
+	return (0);
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,13 +6,14 @@
 /*   By: sanghupa <sanghupa@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 23:23:40 by sanghupa          #+#    #+#             */
-/*   Updated: 2022/12/16 23:26:45 by sanghupa         ###   ########.fr       */
+/*   Updated: 2022/12/20 12:14:20 by sanghupa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <unistd.h>
 # include <stdlib.h>
 
 char	*get_next_line(int fd);

4262513fb99f606c1bf6fef30b05b7843553edbf
Sanghun <po4865@gmail.com>
2022-12-24 23:19:24 +0100

    feat: get_next_line bonus DONE

--- get_next_line/get_next_line_bonus.h
+++ get_next_line/get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: sanghupa <sanghupa@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 23:23:40 by sanghupa          #+#    #+#             */
-/*   Updated: 2022/12/20 12:16:31 by sanghupa         ###   ########.fr       */
+/*   Updated: 2022/12/24 22:12:02 by sanghupa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,18 @@
 # include <unistd.h>
 # include <stdlib.h>
 
+// main
+
 char	*get_next_line(int fd);
+char	*ft_add_bufs(int fd, char *bufs);
+char	*ft_get_line(char *bufs);
+char	*ft_update_bufs(char *bufs);
+
+// utils
+
+char	*ft_strchr_gnl(char *s, int c);
+char	*ft_strdup_gnl(char *s1);
+char	*ft_strjoin_gnl(char *s1, char *s2);
+size_t	ft_strlen_gnl(char *s);
 
 #endif

abc166d0cab26df1713351a572d7dba147e77cab
Sanghun <po4865@gmail.com>
2022-12-31 22:29:19 +0100

    init: Initiate minitalk project
    - Create `Makefile`.
    - Create .h files.
    - Create .c files.

---
+++ minitalk/include/minitalk.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sanghupa <sanghupa@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/31 21:55:46 by sanghupa          #+#    #+#             */
+/*   Updated: 2022/12/31 22:12:17 by sanghupa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <signal.h>
+# include "libft.h"
+
+/* server.c */
+
+/* client.c */
+
+#endif
---
+++ minitalk/include/minitalk_bonus.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk_bonus.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sanghupa <sanghupa@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/31 21:55:46 by sanghupa          #+#    #+#             */
+/*   Updated: 2022/12/31 22:10:36 by sanghupa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_BONUS_H
+# define MINITALK_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <signal.h>
+# include "libft.h"
+
+/* server.c */
+
+/* client.c */
+
+#endif

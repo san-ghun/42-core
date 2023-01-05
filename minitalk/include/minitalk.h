6b915d31df999ebc943d2fcadd8b87e36f82bd28
Sanghun <po4865@gmail.com>
2023-01-05 11:34:38 +0100

    feat: Configure to make without libft
    - Modify Makefile and header files to compile and run program without libft.

--- minitalk/include/minitalk.h
+++ minitalk/include/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: sanghupa <sanghupa@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/31 21:55:46 by sanghupa          #+#    #+#             */
-/*   Updated: 2022/12/31 22:12:17 by sanghupa         ###   ########.fr       */
+/*   Updated: 2023/01/04 21:59:02 by sanghupa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <signal.h>
-# include "libft.h"
+// # include "libft.h"
+
+# include <stdio.h>
 
 /* server.c */
 

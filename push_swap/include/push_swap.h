54f000f177859794c8767a91546011825d137614
Sanghun Park <sanghupa@student.42berlin.de>
2023-05-05 13:00:08 +0200

    init: Initiate push_swap

---
+++ push_swap/include/push_swap.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sanghupa <sanghupa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/05 12:32:20 by sanghupa          #+#    #+#             */
+/*   Updated: 2023/05/05 12:43:55 by sanghupa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include "libft.h"
+# include "ft_printf.h"
+
+# include <stdio.h>
+# include <ctype.h>
+
+/* push_swap_val_input.c */
+/* push_swap_rules.c */
+/* push_swap_sort.c */
+
+/* push_swap_error.c */
+
+int	ft_str_isdigit(char *str);
+
+int	ft_error_argv(void);
+
+int	ft_error_argc(void);
+
+/* push_swap.c */
+
+#endif

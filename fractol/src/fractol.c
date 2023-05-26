8e0fb15e7e3765c1e09489eb4165b7386d1aa292
Sanghun Park <sanghupa@student.42berlin.de>
2023-05-26 15:34:21 +0200

    feat: Update fractol.c to fit norm test

--- fractol/src/fractol.c
+++ fractol/src/fractol.c
@@ -6,21 +6,21 @@
 /*   By: sanghupa <sanghupa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 15:48:02 by sanghupa          #+#    #+#             */
-/*   Updated: 2023/05/19 15:54:00 by sanghupa         ###   ########.fr       */
+/*   Updated: 2023/05/26 15:24:27 by sanghupa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
 // Exit the program as failure.
-static void ft_error(void)
+static void	ft_error(void)
 {
 	fprintf(stderr, "%s", mlx_strerror(mlx_errno));
 	exit(EXIT_FAILURE);
 }
 
 // Print the window width and height.
-static void ft_hook(void *param)
+static void	ft_hook(void *param)
 {
 	const mlx_t	*mlx = param;
 
@@ -28,18 +28,15 @@ static void ft_hook(void *param)
 }
 
 // Hook for key input
-static void ft_keyhook(mlx_key_data_t keydata, void *param) 
+static void	ft_keyhook(mlx_key_data_t keydata, void *param)
 {
 	(void)param;
-
 	// If we PRESS the 'J' key, print "Hello".
 	if (keydata.key == MLX_KEY_J && keydata.action == MLX_PRESS)
 		puts("Hello ");
-
 	// If we RELEASE the 'K' key, print "World".
 	if (keydata.key == MLX_KEY_K && keydata.action == MLX_RELEASE)
 		puts("World");
-
 	// If we HOLD the 'L' key, print "!".
 	if (keydata.key == MLX_KEY_L && keydata.action == MLX_REPEAT)
 		puts("!");
@@ -47,25 +44,23 @@ static void ft_keyhook(mlx_key_data_t keydata, void *param)
 
 int	main(int argc, char **argv)
 {
+	mlx_t		*mlx;
+	mlx_image_t	*img;
+
 	(void)argc;
 	(void)argv;
-
 	// MLX allows you to define its core behaviour before startup.
 	mlx_set_setting(MLX_MAXIMIZED, true);
-	mlx_t	*mlx = mlx_init(WIDTH, HEIGHT, "42s", true);
+	mlx = mlx_init(WIDTH, HEIGHT, "42s", true);
 	if (!mlx)
 		ft_error();
-
 	/* Do stuff */
-
 	// Create and display the image.
-	mlx_image_t	*img = mlx_new_image(mlx, 256, 256);
+	img = mlx_new_image(mlx, 256, 256);
 	if (!img || (mlx_image_to_window(mlx, img, 0, 0) < 0))
 		ft_error();
-
 	// Even after the image is being displayed, we can still modify the buffer.
 	mlx_put_pixel(img, 0, 0, 0xFF0000FF);
-
 	// Register a hook and pass mlx as an optional param.
 	// NOTE: Do this before calling mlx_loop!
 	mlx_key_hook(mlx, &ft_keyhook, NULL);

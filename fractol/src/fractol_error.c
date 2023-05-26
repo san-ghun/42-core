1039f9c93df9bcbbae37bbe288c962099d5cef83
Sanghun Park <sanghupa@student.42berlin.de>
2023-05-26 17:01:24 +0200

    feat: Edit error message text output

--- fractol/src/fractol_error.c
+++ fractol/src/fractol_error.c
@@ -6,7 +6,7 @@
 /*   By: sanghupa <sanghupa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 16:17:35 by sanghupa          #+#    #+#             */
-/*   Updated: 2023/05/12 16:18:32 by sanghupa         ###   ########.fr       */
+/*   Updated: 2023/05/26 16:11:15 by sanghupa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,12 @@ int	ft_str_isdigit(char *str)
 
 int	ft_error_argv(void)
 {
-	ft_printf("Error: Invalid argument - argv\n");
+	ft_printf("\nError: Invalid argument - argv\n\n");
 	return (1);
 }
 
 int	ft_error_argc(void)
 {
-	ft_printf("Error: Invalid argument - argc\n");
+	ft_printf("\nError: Invalid argument - argc\n\n");
 	return (1);
 }

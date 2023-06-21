1f9fd031ed1368a1eba06573d4d8bad8b2d16a49
Sanghun <po4865@gmail.com>
2023-06-21 12:19:49 +0200

    feat: Add doxygen comment for struct & func - Update push_swap.h file.

--- push_swap/include/push_swap.h
+++ push_swap/include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: sanghupa <sanghupa@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 17:48:59 by sanghupa          #+#    #+#             */
-/*   Updated: 2023/06/19 14:04:41 by sanghupa         ###   ########.fr       */
+/*   Updated: 2023/06/21 12:14:54 by sanghupa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,11 @@
 
 # define MAX_DLST	512
 
+/// @struct t_dlst
+/// @brief Doubly Linked List Node.
+/// This structure represents a node of a doubly linked list.
+/// It contains information about the content and index of the node,
+/// as well as pointers to the previous and next nodes in the list.
 typedef struct s_dlst
 {
 	int				content;
@@ -39,32 +44,81 @@ typedef struct s_dlst
 
 /* push_swap_dlst_create.c */
 
+/// @brief Create a new node for a doubly linked list.
+/// This function creates a new node for a doubly linked list 
+/// and initializes its members.
+/// @param content The content to be stored in the node.
+/// @return A pointer to the newly created node (t_dlst*) on success, 
+/// or NULL if memory allocation fails.
 t_dlst	*ft_dlstnew(int content);
 
+/// @brief Add a node to the front of a doubly linked list.
+/// This function adds a new node to the front of a doubly linked list.
+/// @param dlst A pointer to a pointer to the head of the doubly linked list.
+/// @param new A pointer to the node to be added.
 void	ft_dlstadd_front(t_dlst *dlst[], t_dlst *new);
 
+/// @brief Add a node to the end of a doubly linked list.
+/// This function adds a new node to the end of a doubly linked list.
+/// @param dlst A pointer to a pointer to the head of the doubly linked list.
+/// @param new A pointer to the node to be added.
 void	ft_dlstadd_back(t_dlst *dlst[], t_dlst *new);
 
 /* push_swap_dlst_read.c */
 
+/// @brief Get a node with a specific content from a doubly linked list.
+/// This function searches for a node 
+/// with a specific content in a doubly linked list.
+/// @param dlst A pointer to a pointer to the head of the doubly linked list.
+/// @param content The content to search for.
+/// @return A pointer to the found node (t_dlst*) if found, 
+/// or NULL if not found or if the list is empty.
 t_dlst	*ft_dlstget(t_dlst *dlst[], int content);
 
 /* push_swap_dlst_update.c */
 
+/// @brief Update the content of a node in a doubly linked list.
+/// This function updates the content of a node 
+/// with a new value in a doubly linked list.
+/// @param dlst A pointer to a pointer to the head of the doubly linked list.
+/// @param old The old content value to search for.
+/// @param new The new content value to update with.
 void	ft_dlstupdate(t_dlst *dlst[], int old, int new);
 
 /* push_swap_dlst_delete.c */
 
-void	ft_dlstdelone(t_dlst *dlst);
+/// @brief Delete a single node from a doubly linked list.
+/// This function deletes a single node from a doubly linked list.
+/// @param dlst A pointer to a pointer to the head of the doubly linked list.
+/// @param elem A pointer to the node to be deleted.
+void	ft_dlstdelone(t_dlst *dlst[], t_dlst *elem);
 
+
+/// @brief Clear a doubly linked list and free all nodes.
+/// This function clears a doubly linked list by freeing all its nodes.
+/// @param dlst dlst A pointer to a pointer to the head 
+/// of the doubly linked list.
 void	ft_dlstclear(t_dlst *dlst[]);
 
 /* push_swap_dlst_util.c */
 
+/// @brief Get the size of a doubly linked list.
+/// This function calculates and returns the number of nodes 
+/// in a doubly linked list.
+/// @param dlst 
+/// @return 
 int		ft_dlstsize(t_dlst *dlst);
 
+/// @brief Get the last node of a doubly linked list.
+/// This function returns a pointer to the last node of a doubly linked list.
+/// @param dlst A pointer to the head of the doubly linked list.
+/// @return A pointer to the last node of the doubly linked list.
 t_dlst	*ft_dlstlast(t_dlst *dlst);
 
+/// @brief Print the contents of a doubly linked list
+/// This function prints the contents of a doubly linked list.
+/// @param dlst dlst A pointer to a pointer to the head 
+/// of the doubly linked list.
 void	ft_dlstprint(t_dlst *dlst[]);
 
 /* push_swap_error.c */

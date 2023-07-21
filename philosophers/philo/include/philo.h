82604464a2fbd0e3edac34345bd2cfa833fc3094
Sanghun <po4865@gmail.com>
2023-07-21 22:17:19 +0200

    config: Configure file structure for philo and philo_bonus - Create and edit Makefiles. - Create and add barebone .h and .c files.

---
+++ philosophers/philo/include/philo.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sanghupa <sanghupa@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/21 22:07:52 by sanghupa          #+#    #+#             */
+/*   Updated: 2023/07/21 22:12:18 by sanghupa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+// Limit Loop: use addition to while condition
+# define MAX_ITER	5000
+
+// Prevent Heap mem leak: use addition to char or array
+# define DATA_SIZE	3072
+
+/* philo.c */
+
+/* philo_util.c */
+
+#endif
---
+++ philosophers/philo_bonus/include/philo_bonus.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_bonus.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sanghupa <sanghupa@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/21 22:07:55 by sanghupa          #+#    #+#             */
+/*   Updated: 2023/07/21 22:10:10 by sanghupa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_BONUS_H
+# define PHILO_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+// Limit Loop: use addition to while condition
+# define MAX_ITER	5000
+
+// Prevent Heap mem leak: use addition to char or array
+# define DATA_SIZE	3072
+
+/* philo_bonus.c */
+
+#endif

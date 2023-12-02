a6f848b0a1fb3c96d7c744127f87712887203806
Sanghun Park <sanghupa@student.42berlin.de>
2023-12-02 17:48:14 +0100

    make: Optimize setting for compile of executable with libraries

--- minirt/include/minirt.h
+++ minirt/include/minirt.h
@@ -6,19 +6,52 @@
 /*   By: sanghupa <sanghupa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 14:23:17 by sanghupa          #+#    #+#             */
-/*   Updated: 2023/12/02 14:25:05 by sanghupa         ###   ########.fr       */
+/*   Updated: 2023/12/02 17:20:35 by sanghupa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINIRT_H
 # define MINIRT_H
 
+/*
+** =============================================================================
+** Dependencies
+** =============================================================================
+*/
+
+/// open, close
+# include <fcntl.h>
+
+/// read, write
 # include <unistd.h>
+
+/// printf, perror
+# include <stdio.h>
+
+/// strerror
+# include <string.h>
+
+/// malloc, free, exit
 # include <stdlib.h>
+
 # include <math.h>
+# include <stdint.h>
+# include <pthread.h>
+
+// =============================================================================
+
+/// minilibx
 # include "mlx_int.h"
 # include "mlx.h"
+
+/// libft
 # include "libft.h"
+# include "get_next_line.h"
 # include "ft_printf.h"
+# include "from_ms.h"
+
+/// extra
+# include "key_linux.h"
+// # include "key_macos.h"
 
 #endif
--- minirt/src/main.c
+++ minirt/src/main.c
@@ -6,7 +6,7 @@
 /*   By: sanghupa <sanghupa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 14:22:52 by sanghupa          #+#    #+#             */
-/*   Updated: 2023/12/02 15:10:16 by sanghupa         ###   ########.fr       */
+/*   Updated: 2023/12/02 17:11:44 by sanghupa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,11 @@ int	main(int argc, char *argv[])
 {
 	(void)argc;
 	(void)argv;
+	// check arguments
+	// initialize mlx
+	// initialize minirt
+	// draw minirt
+	// extra mlx
+	// etc.
 	return (0);
 }

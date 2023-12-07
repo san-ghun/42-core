4e2fca86259af71016402684d29531342a5fdcf1
Sanghun Park <sanghupa@student.42berlin.de>
2023-12-07 21:13:00 +0100

    feat: Update main.c file to be able to compile

--- minirt/src/main.c
+++ minirt/src/main.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sanghupa <sanghupa@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: sanghupa <sanghupa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 14:22:52 by sanghupa          #+#    #+#             */
-/*   Updated: 2023/12/02 23:51:01 by sanghupa         ###   ########.fr       */
+/*   Updated: 2023/12/07 21:10:50 by sanghupa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-typedef struct	s_data {
+typedef struct s_data {
 	void	*img;
 	char	*addr;
 	int		bits_per_pixel;
@@ -20,7 +20,7 @@ typedef struct	s_data {
 	int		endian;
 }				t_data;
 
-typedef struct	s_vars {
+typedef struct s_vars {
 	void	*mlx;
 	void	*win;
 }				t_vars;
@@ -40,14 +40,12 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	char	*dst;
 
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
+	*(unsigned int *)dst = color;
 }
 
 int	main(int argc, char *argv[])
 {
 	t_vars	vars;
-	void	*mlx;
-	void	*mlx_win;
 	t_data	img;
 
 	(void)argc;
@@ -60,8 +58,9 @@ int	main(int argc, char *argv[])
 	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
 	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
 
-	mlx_hook(vars.win, 3, 1L<<0, close_mlx, &vars);
+	mlx_key_hook(vars.win, close_mlx, &vars);
 	mlx_loop(vars.mlx);
+
 	// check arguments
 	// initialize mlx
 	// initialize minirt

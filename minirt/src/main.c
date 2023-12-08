00a7879d2cf2443df5480845b56566d466d0b22d
Sanghun Park <sanghupa@student.42berlin.de>
2023-12-08 18:07:01 +0100

    feat: Add draw_square() function to draw object

--- minirt/src/main.c
+++ minirt/src/main.c
@@ -6,11 +6,29 @@
 /*   By: sanghupa <sanghupa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 14:22:52 by sanghupa          #+#    #+#             */
-/*   Updated: 2023/12/08 17:48:41 by sanghupa         ###   ########.fr       */
+/*   Updated: 2023/12/08 18:05:12 by sanghupa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
+
+void	draw_square(t_square square, t_data *img)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	while (i < square.size && i + square.y < img->h)
+	{
+		j = 0;
+		while (j < square.size && j + square.x < img->w)
+		{
+			put_pixel_data(img, j + square.x, i + square.y, square.color);
+			j++;
+		}
+		i++;
+	}
+}
 
 int	main(int argc, char *argv[])
 {
@@ -24,6 +42,7 @@ int	main(int argc, char *argv[])
 		return (1);
 	img = new_img(300, 300, vars);
 
+	draw_square((t_square){20, 20, 260, 0x0000FF}, img);
 	put_pixel_data(img, 150, 150, 0x00FFFFF);
 	mlx_put_image_to_window(vars->mlx, vars->win, img->img, 0, 0);
 
